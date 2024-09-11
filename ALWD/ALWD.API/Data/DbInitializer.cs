d166363d7d0f660c431569847a5f1019c0e4d9ad
KirillKurril <kirillkurill@gmail.com>
2024-09-12 00:00:51 +0300

    как хитро получается

--- ALWD/ALWD.API/Data/DbInitializer.cs
+++ ALWD/ALWD.API/Data/DbInitializer.cs
@@ -36,34 +36,34 @@ namespace ALWD.API.Data
 			var products = new List<Product>
 			{
 			new Product {Name="Стартер A", Description="Описание стартера A", Price=120.00, Quantity=10, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("starters"))},
-			new Product {Name="Стартер B", Description="Описание стартера B", Price=130.00, Quantity=8, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("starters"))},
+			new Product {Name="Стартер B", Description="Описание стартера B", Price=130.00, Quantity=8, ImagePath=baseUri + "/Image/стартер_b.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("starters"))},
 
-			new Product {Name="Тормозной диск A", Description="Описание тормозного диска A", Price=75.00, Quantity=20, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("brake-discs"))},
-			new Product {Name="Тормозной диск B", Description="Описание тормозного диска B", Price=80.00, Quantity=15, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("brake-discs"))},
+			new Product {Name="Тормозной диск A", Description="Описание тормозного диска A", Price=75.00, Quantity=20, ImagePath=baseUri + "/Image/тормозной_диск_a.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("brake-discs"))},
+			new Product {Name="Тормозной диск B", Description="Описание тормозного диска B", Price=80.00, Quantity=15, ImagePath=baseUri + "/Image/тормозной_диск_b.jpg", ImageMimeType="image/jpeg", Category=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("brake-discs"))},
 
-			new Product {Name="Фильтр A", Description="Описание фильтра A", Price=15.00, Quantity=50, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("filters")).Id },
-			new Product {Name="Фильтр B", Description="Описание фильтра B", Price=18.00, Quantity=40, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("filters")).Id },
+			new Product {Name="Фильтр A", Description="Описание фильтра A", Price=15.00, Quantity=50, ImagePath=baseUri + "/Image/фильтр_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("filters")).Id },
+			new Product {Name="Фильтр B", Description="Описание фильтра B", Price=18.00, Quantity=40, ImagePath=baseUri + "/Image/фильтр_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("filters")).Id },
 
-			new Product {Name="Свеча зажигания A", Description="Описание свечи A", Price=5.00, Quantity=100, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("spark-plugs")).Id },
-			new Product {Name="Свеча зажигания B", Description="Описание свечи B", Price=6.00, Quantity=90, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("spark-plugs")).Id },
+			new Product {Name="Свеча зажигания A", Description="Описание свечи A", Price=5.00, Quantity=100, ImagePath=baseUri + "/Image/свеча_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("spark-plugs")).Id },
+			new Product {Name="Свеча зажигания B", Description="Описание свечи B", Price=6.00, Quantity=90, ImagePath=baseUri + "/Image/свеча_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("spark-plugs")).Id },
 
-			new Product {Name="Амортизатор A", Description="Описание амортизатора A", Price=50.00, Quantity=30, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("shock-absorbers")).Id },
-			new Product {Name="Амортизатор B", Description="Описание амортизатора B", Price=55.00, Quantity=25, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("shock-absorbers")).Id },
+			new Product {Name="Амортизатор A", Description="Описание амортизатора A", Price=50.00, Quantity=30, ImagePath=baseUri + "/Image/амортизатор_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("shock-absorbers")).Id },
+			new Product {Name="Амортизатор B", Description="Описание амортизатора B", Price=55.00, Quantity=25, ImagePath=baseUri + "/Image/амортизатор_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("shock-absorbers")).Id },
 
-			new Product {Name="Масло A", Description="Описание масла A", Price=25.00, Quantity=60, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("oils-lubricants")).Id },
-			new Product {Name="Масло B", Description="Описание масла B", Price=30.00, Quantity=55, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("oils-lubricants")).Id },
+			new Product {Name="Масло A", Description="Описание масла A", Price=25.00, Quantity=60, ImagePath=baseUri + "/Image/масло_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("oils-lubricants")).Id },
+			new Product {Name="Масло B", Description="Описание масла B", Price=30.00, Quantity=55, ImagePath=baseUri + "/Image/масло_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("oils-lubricants")).Id },
 
-			new Product {Name="Шина A", Description="Описание шины A", Price=100.00, Quantity=20, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("tires-wheels")).Id },
-			new Product {Name="Шина B", Description="Описание шины B", Price=110.00, Quantity=18, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("tires-wheels")).Id },
+			new Product {Name="Шина A", Description="Описание шины A", Price=100.00, Quantity=20, ImagePath=baseUri + "/Image/шина_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("tires-wheels")).Id },
+			new Product {Name="Шина B", Description="Описание шины B", Price=110.00, Quantity=18, ImagePath=baseUri + "/Image/шина_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("tires-wheels")).Id },
 
-			new Product {Name="Аксессуар A", Description="Описание аксессуара A", Price=20.00, Quantity=70, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("electronics-accessories")).Id },
-			new Product {Name="Аксессуар B", Description="Описание аксессуара B", Price=25.00, Quantity=65, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("electronics-accessories")).Id },
+			new Product {Name="Аксессуар A", Description="Описание аксессуара A", Price=20.00, Quantity=70, ImagePath=baseUri + "/Image/аксессуар_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("electronics-accessories")).Id },
+			new Product {Name="Аксессуар B", Description="Описание аксессуара B", Price=25.00, Quantity=65, ImagePath=baseUri + "/Image/аксессуар_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("electronics-accessories")).Id },
 
-			new Product {Name="Радиатор A", Description="Описание радиатора A", Price=150.00, Quantity=10, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("cooling-system")).Id },
-			new Product {Name="Радиатор B", Description="Описание радиатора B", Price=160.00, Quantity=8, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("cooling-system")).Id },
+			new Product {Name="Радиатор A", Description="Описание радиатора A", Price=150.00, Quantity=10, ImagePath=baseUri + "/Image/радиатор_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("cooling-system")).Id },
+			new Product {Name="Радиатор B", Description="Описание радиатора B", Price=160.00, Quantity=8, ImagePath=baseUri + "/Image/радиатор_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("cooling-system")).Id },
 
-			new Product {Name="Бампер A", Description="Описание бампера A", Price=200.00, Quantity=5, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("exterior-parts")).Id },
-			new Product {Name="Бампер B", Description="Описание бампера B", Price=220.00, Quantity=4, ImagePath=baseUri + "/Image/стартер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("exterior-parts")).Id }
+			new Product {Name="Бампер A", Description="Описание бампера A", Price=200.00, Quantity=5, ImagePath=baseUri + "/Image/бампер_a.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("exterior-parts")).Id },
+			new Product {Name="Бампер B", Description="Описание бампера B", Price=220.00, Quantity=4, ImagePath=baseUri + "/Image/бампер_b.jpg", ImageMimeType="image/jpeg", CategoryId=context.Categories.FirstOrDefault(c=>c.NormalizedName.Equals("exterior-parts")).Id }
 			};
 			context.Products.AddRange(products);
 
--- ALWD/ALWD.Domain/obj/Debug/net8.0/ALWD.Domain.AssemblyInfo.cs
+++ ALWD/ALWD.Domain/obj/Debug/net8.0/ALWD.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ALWD.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0114b4aa9b5a1e76e28c1e2a3c8bd7ad6993ea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a04e579365caf05d5d6e8a3aba8736424355de")]
 [assembly: System.Reflection.AssemblyProductAttribute("ALWD.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ALWD.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
